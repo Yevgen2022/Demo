e6a31ce4b011398d5d0733023f8eac0738985be1
Yevhen <yoshkukov@Yevhens-MBP.localdomain>
2024-11-27 15:55:58 +0100

    lesson34 do not work redirect

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -8,26 +8,26 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-
-    $note = $db->query('select * from notes where id = :id', [
-        'id' => $_GET['id']
-    ])->findOrFail();
-
-    authorize($note['user_id'] === $currentUserId);
-
-
-
-    $db->query('DELETE FROM notes WHERE id = :id', [
-        'id' => $_GET['id']
-    ]);
-
-
-    header('Location: /notes');
-    exit();
-
-} else {
+//if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//
+//
+//    $note = $db->query('select * from notes where id = :id', [
+//        'id' => $_GET['id']
+//    ])->findOrFail();
+//
+//    authorize($note['user_id'] === $currentUserId);
+//
+//
+//
+//    $db->query('DELETE FROM notes WHERE id = :id', [
+//        'id' => $_GET['id']
+//    ]);
+//
+//
+//    header('Location: /notes');
+//    exit();
+//
+//} else {
 
     $note = $db->query('select * from notes where id = :id', [
         'id' => $_GET['id']
@@ -39,4 +39,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'heading' => 'Note',
         'note' => $note
     ]);
-}
+//}
---
+++ controllers/notes/store.php
@@ -0,0 +1,36 @@
+<?php
+
+use Core\Validator;
+use Core\Database;
+
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+
+$errors = [];
+
+if (!Validator::string($_POST['body'], 1, 100)) {
+    $errors['body'] = 'A body of no more then 100 characters is required.';
+}
+
+
+if (! empty($errors)) {
+    return view("notes/create.view.php", [
+        'heading' => 'Create Note',
+        'errors' => $errors
+    ]);
+
+}
+
+
+//if (empty($errors)) {
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
+//}
+    header('location: /notes/');
+    die();
+
+
