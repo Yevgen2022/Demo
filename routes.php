f6587288bd25bf54ade5707ff891ce5246ae85b9
Yevhen <yoshkukov@Yevhens-MBP.localdomain>
2025-01-02 11:30:17 +0100

    Create dir-Registration and file-create.php

--- routes.php
+++ routes.php
@@ -1,9 +1,7 @@
 <?php
 
 
-
-
-
+global $router;
 $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
@@ -18,4 +16,4 @@ $router->patch('/note', 'controllers/notes/update.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
 
-
+$router->get('/register', 'controller/registration/create.php');
