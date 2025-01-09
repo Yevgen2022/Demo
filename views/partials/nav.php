6180e8a07a95058ff2f03221104845e44a0bb079
Yevhen <yoshkukov@Yevhens-MBP.localdomain>
2025-01-09 14:11:43 +0100

    Create 2-routers and 2-controller,create and store, and session/create.view.php

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -57,7 +57,10 @@
                             </button>
 
                             <?php if (!($_SESSION['user'] ?? false)) : ?>
-                                <a href="/register" class="text-white">Register</a>
+<!--                                <a href="/register" class="text-white">Register</a>-->
+        <a href="/register" class="<?= urlIs('/register') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?>  hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md  text-sm font-medium ">Register</a>
+        <a href="/login" class="<?= urlIs('/login') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?>  hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md  text-sm font-medium ">Log in</a>
+
                             <?php endif; ?>
 
 
